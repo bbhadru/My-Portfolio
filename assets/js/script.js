078ecaa14d5ca8e4c9d38d09889c62797dd172fb
VeerannaBanothu <veerannabanothu766@gmail.com>
2026-09-03 00:28:05 +0530

    UpdatedFiles

--- assets/js/script.js
+++ assets/js/script.js
@@ -59,7 +59,7 @@ $(document).ready(function () {
 document.addEventListener('visibilitychange',
     function () {
         if (document.visibilityState === "visible") {
-            document.title = "Portfolio | UshaRani Dharavath";
+            document.title = "Portfolio | Bhadru Banothu";
             $("#favicon").attr("href", "");
         }
         else {
